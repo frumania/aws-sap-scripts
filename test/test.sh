3e62b31a6dfc23d99a3c19d03d2371d70baad595
frumania <info@frumania.com>
2019-06-09 23:14:25 +0200

    init

--- test/test.sh
+++ test/test.sh
@@ -11,14 +11,14 @@ echo "Test Cloud Connector"
 sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --document-version "1" --targets "Key=instanceids,Values=$instance_id" --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\n\"owner\":\"frumania\",\n\"repository\":\"aws-sap-scripts\",\n\"path\":\"cloud_connector\"\n}"],"commandLine":["deploy.sh"],"workingDirectory":[""],"executionTimeout":["3600"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output-s3-bucket-name "aws-ssm-instance-logs" --region eu-central-1 --output text --query "Command.CommandId")
 echo "Wait 4 min"
 sleep 300
-result=$(aws ssm list-command-invocations --command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].Status")
+result=$(aws ssm list-command-invocations --instance-id $instance_id --command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].Status")
 echo $result
 
 echo "Test HANA SDA Athena"
 sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --document-version "1" --targets "Key=instanceids,Values=$instance_id" --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\n\"owner\":\"frumania\",\n\"repository\":\"aws-sap-scripts\",\n\"path\":\"hana_sda_athena\"\n}"],"commandLine":["deploy.sh s3://aws-athena-hana-int/logs/ eu-central-1"],"workingDirectory":[""],"executionTimeout":["3600"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output-s3-bucket-name "aws-ssm-instance-logs" --region eu-central-1 --output text --query "Command.CommandId")
 echo "Wait 4 min"
 sleep 300
-result=$(aws ssm list-command-invocations --command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].{Status:Status,Output:Output}")
+result=$(aws ssm list-command-invocations --instance-id $instance_id--command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].Status-")
 echo $result
 
 #echo "Test HANA Cockpit"
