b2884c2b2180e64c8bb103a6c9bfd2734082db23
frumania <info@frumania.com>
2019-06-10 13:49:06 +0200

    init

--- hana_studio/deploy.bat
+++ hana_studio/deploy.bat
@@ -1,19 +1,22 @@
-
 mkdir c:\tmp
 cd /tmp
 Read-S3Object -BucketName sap-sources -KeyPrefix HANA_CLIENT/HANA_STUDIO -Folder /tmp
-.\jre-8u211-windows-x64.exe /s
 
-Start-Process 'C:\tmp\jre-8u211-windows-x64.exe' -ArgumentList 'INSTALL_SILENT=Enable REBOOT=Disable SPONSORS=Disable' -Wait -PassThru
+amazon-corretto-8.212.04.2-windows-x64.msi /qn /L* "C:\tmp\install.log" /norestart ALLUSERS=2
 
+sleep 60
 
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Amazon Corretto\jdk1.8.0_212")
 
-$text='
-INSTALL_SILENT=Enable
-AUTO_UPDATE=Enable
-SPONSORS=Disable
-REMOVEOUTOFDATEJRES=1
-'
-$text | Set-Content "c:\tmp\jreinstall.cfg"
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$($Env:JAVA_HOME)\bin", "User")
 
-Start-Process -FilePath "c:\tmp\jre-8u211-windows-x64.exe" -ArgumentList INSTALLCFG="c:\tmp\jreinstall.cfg"
+sudo /usr/local/eclipse/eclipse -nosplash \
+  -application org.eclipse.equinox.p2.director \
+  -repository http://download.eclipse.org/releases/indigo/,http://download.eclipse.org/tools/cdt/releases/helios/ \
+  -destination /usr/local/eclipse \
+  -installIU org.eclipse.cdt.feature.group \
+  -installIU org.eclipse.cdt.sdk.feature.group \
+  -installIU org.eclipse.cdt.platform.feature.group \
+  -installIU org.eclipse.cdt.debug.ui.memory.feature.group \
+  -installIU org.eclipse.cdt.debug.edc.feature.group \
+  -installIU org.eclipse.cdt.util.feature.group
