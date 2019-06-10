a4ce22838aa4d880cca85356c7432e8255298bdb
frumania <info@frumania.com>
2019-06-10 12:56:30 +0200

    init

--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,8 @@ set -e
 echo "Boot Instance"
 instance_id=$(aws ec2 run-instances --launch-template LaunchTemplateId=lt-0224bf3ab77c362a2,Version=7 --query 'Instances[].[InstanceId]' --output text)
 echo $instance_id
-echo "Wait 3 min"
-sleep 180
+echo "Wait 4 min"
+sleep 240
 
 echo "Test Cloud Connector"
 sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --document-version "1" --targets "Key=instanceids,Values=$instance_id" --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\n\"owner\":\"frumania\",\n\"repository\":\"aws-sap-scripts\",\n\"path\":\"cloud_connector\"\n}"],"commandLine":["deploy.sh"],"workingDirectory":[""],"executionTimeout":["3600"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output-s3-bucket-name "aws-ssm-instance-logs" --region eu-central-1 --output text --query "Command.CommandId")
