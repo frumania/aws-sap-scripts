3822588ecd972eabbb7992ef7fcdb1bfda82b79b
frumania <info@frumania.com>
2019-06-10 12:40:48 +0200

    init

--- test/test.sh
+++ test/test.sh
@@ -9,9 +9,10 @@ sleep 240
 
 echo "Test Cloud Connector"
 sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --document-version "1" --targets "Key=instanceids,Values=$instance_id" --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\n\"owner\":\"frumania\",\n\"repository\":\"aws-sap-scripts\",\n\"path\":\"cloud_connector\"\n}"],"commandLine":["deploy.sh"],"workingDirectory":[""],"executionTimeout":["3600"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output-s3-bucket-name "aws-ssm-instance-logs" --region eu-central-1 --output text --query "Command.CommandId")
+echo $sh_command_id
 echo "Wait 3 min"
 sleep 240
-result=$(aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].Status")
+result=$(aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --details --query "Command.Status")
 echo $result
 
 if [[ $result != *"success"* ]]; then
@@ -21,6 +22,7 @@ fi
 
 echo "Test HANA SDA Athena"
 sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --document-version "1" --targets "Key=instanceids,Values=$instance_id" --parameters '{"sourceType":["GitHub"],"sourceInfo":["{\n\"owner\":\"frumania\",\n\"repository\":\"aws-sap-scripts\",\n\"path\":\"hana_sda_athena\"\n}"],"commandLine":["deploy.sh s3://aws-athena-hana-int/logs/ eu-central-1"],"workingDirectory":[""],"executionTimeout":["3600"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --output-s3-bucket-name "aws-ssm-instance-logs" --region eu-central-1 --output text --query "Command.CommandId")
+echo $sh_command_id
 echo "Wait 3 min"
 sleep 240
 result=$(aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --details --query "CommandInvocations[].CommandPlugins[].Status")
