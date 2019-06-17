ab7d6de1bb68bd2076b1ed3555efb8507efef9f9
frumania <info@frumania.com>
2019-06-17 11:29:20 +0200

    init

--- test/test_win.sh
+++ test/test_win.sh
@@ -27,7 +27,7 @@ sh_command_id=$(aws ssm send-command --document-name "AWS-RunRemoteScript" --doc
 echo $sh_command_id
 echo "Wait 3 min"
 sleep 180
-aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --plugin-name runPowerShellScript --output text --query "Status"
+#aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --plugin-name runPowerShellScript --output text --query "Status"
 #result=$(aws ssm get-command-invocation --instance-id $instance_id --command-id "$sh_command_id" --plugin-name runPowerShellScript --output text --query "Status")
 #echo $result
 #if [[ $result != *"Success"* ]]; then
