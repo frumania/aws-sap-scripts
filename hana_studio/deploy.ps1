ab5231f55284705281906255b718650997ade3b7
Marcel Törpe <info@frumania.com>
2020-07-21 15:00:27 +0200

    Update deploy.ps1

--- hana_studio/deploy.ps1
+++ hana_studio/deploy.ps1
@@ -1,7 +1,7 @@
-echo "START"
+ echo "START"
 $ErrorActionPreference = "Stop"
 
-$mypath = "c:/eclipse2018-12"
+$mypath = "c:/eclipse2020-06"
 If(!(test-path $mypath))
 {
       New-Item -ItemType Directory -Force -Path $mypath
@@ -9,67 +9,38 @@ If(!(test-path $mypath))
 
 cd $mypath
 
-echo "Downloading Amazon Corretto..."
-$url = "https://d3pxv6yz143wms.cloudfront.net/8.212.04.2/amazon-corretto-8.212.04.2-1-windows-x64.msi"
-$output = $mypath+"/amazon-corretto-8.212.04.2-1-windows-x64.msi"
+echo "Downloading Amazon Corretto 8..."
+$url = "https://corretto.aws/downloads/latest/amazon-corretto-8-x64-windows-jdk.msi"
+$output = $mypath+"/amazon-corretto-8-x64-windows-jdk.msi"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 echo "...done!"
-echo "Installing Amazon Corretto..."
-.\amazon-corretto-8.212.04.2-1-windows-x64.msi /qn /L* "install.log" /norestart ALLUSERS=2
+echo "Installing Amazon Corretto 8..."
+.\amazon-corretto-8-x64-windows-jdk.msi /qn /L* "install.log" /norestart ALLUSERS=2
 echo "...done!"
+
+echo "Sleep 60s"
 
 sleep 60
 
 echo "Set JAVA HOME..."
-[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Amazon Corretto\jdk1.8.0_212")
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Amazon Corretto\jre8")
 [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$($Env:JAVA_HOME)\bin", "User")
-#java -version
 echo "...done!"
 
 echo "Downloading Eclipse..."
-$url = "http://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/2018-12/R/eclipse-java-2018-12-R-win32-x86_64.zip"
-$output = $mypath+"/eclipse-java-2018-12-R-win32-x86_64.zip"
+$url = "http://ftp.fau.de/eclipse/technology/epp/downloads/release/2020-06/R/eclipse-java-2020-06-R-win32-x86_64.zip"
+$output = $mypath+"/eclipse-java-2020-06-R-win32-x86_64.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 echo "...done!"
 echo "Installing Eclipse..."
-Expand-Archive -Path eclipse-java-2018-12-R-win32-x86_64.zip -DestinationPath $mypath
+Expand-Archive -Path eclipse-java-2020-06-R-win32-x86_64.zip -DestinationPath $mypath
 echo "...done!"
 
 cd eclipse
 
 dir
 
-echo 'Start-Process -FilePath c:/eclipse2018-12/eclipse/eclipse.exe -ArgumentList "-nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository https://tools.hana.ondemand.com/2018-12,http://download.eclipse.org/releases/2018-12 -installIU HANATools"' > C:\Users\Public\Desktop\Install_SAP_HANA_STUDIO_Plugins.ps1
-
-#echo "Installing SAP HANA Studio Plugin for Eclipse..."
-
-#.\eclipse -nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/2018-12 -installIU "org.eclipse.emf.transaction,org.eclipse.zest.core,org.eclipse.zest.core,org.eclipse.draw2d.feature.group,org.eclipse.emf.common.ui.feature.group,org.eclipse.emf.databinding.feature.group,org.eclipse.emf.ecore.edit.feature.group,org.eclipse.emf.edit.feature.group,org.eclipse.emf.edit.ui.feature.group,org.eclipse.gef.feature.group,org.eclipse.zest.feature.group,org.eclipse.ui.trace,javax.mail,org.apache.commons.collections,org.eclipse.graphiti,org.eclipse.graphiti.ui,org.eclipse.wst.web_ui.feature.feature.group,org.eclipse.emf.feature.group,org.eclipse.emf.transaction.feature.group,org.eclipse.emf.validation.feature.group,org.eclipse.emf.workspace.feature.group,org.eclipse.graphiti.feature.feature.group,org.eclipse.xpand.feature.group,org.eclipse.xtend.ui.feature.group"
-#.\eclipse -nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository https://tools.hana.ondemand.com/2018-12 -list
-#.\eclipse -nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository "https://tools.hana.ondemand.com/2018-12,http://download.eclipse.org/releases/2018-12" -installIU HANATools
-
-#$myeclipsepath = $mypath+"/eclipse/eclipse.exe";
-#Start-Process -FilePath $myeclipsepath -ArgumentList "-nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository https://tools.hana.ondemand.com/2018-12,http://download.eclipse.org/releases/2018-12 -installIU HANATools"
-
-#sleep 360
-
-#echo "...done!"
-
-#Output install log
-#echo "Output install log:"
-#$conf = $mypath+"/eclipse/configuration/";
-#$files = get-childitem -Filter *.log $conf
-#foreach ($file in $files)
-#{
-#  Get-Content -Path $file.Fullname
-#}
-
-#Check Plugins
-#$conf = $mypath+"/eclipse/plugins/";
-#$files = get-childitem -Filter *com.sap.ndb.studio*.jar $conf
-
-#if($files.Length -eq 0)
-#{throw "No SAP HANA plugins found. Installation must have failed!"}
-#else{Write-Host "SAP HANA Plugins found:" $files.Length}
+echo 'Start-Process -FilePath c:/eclipse2020-06/eclipse/eclipse.exe -ArgumentList "-nosplash -consoleLog -application org.eclipse.equinox.p2.director -repository https://tools.hana.ondemand.com/2020-06,http://download.eclipse.org/releases/2020-06 -installIU HANATools"' > C:\Users\Public\Desktop\Install_SAP_HANA_STUDIO_Plugins.ps1
 
 echo "Create Shortcut..."
 
