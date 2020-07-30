405c444d1758106219202e1d9295cb7ddc9034d2
Marcel Törpe <info@frumania.com>
2020-07-30 21:54:04 +0200

    Update deploy.sh

--- cloud_connector/deploy.sh
+++ cloud_connector/deploy.sh
@@ -8,19 +8,19 @@ sudo mkdir /sapcc
 
 cd /sapcc
 
-sudo wget "https://tools.hana.ondemand.com/additional/sapjvm-8.1.055-linux-x64.rpm" --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt"
+sudo wget "https://tools.eu1.hana.ondemand.com/additional/sapjvm-8.1.063-linux-x64.rpm" --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt"
 
-sudo rpm -i sapjvm-8.1.055-linux-x64.rpm
+sudo rpm -i sapjvm-8.1.063-linux-x64.rpm
 
 echo "Install SAP Cloud Connector"
 
-sudo wget "https://tools.hana.ondemand.com/additional/sapcc-2.12.0.1-linux-x64.zip" --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt"
+sudo wget "https://tools.eu1.hana.ondemand.com/additional/sapcc-2.12.4-linux-x64.zip" --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt"
 
 sudo zypper --gpg-auto-import-keys --non-interactive --no-refresh install unzip
 
-sudo unzip sapcc-2.12.0.1-linux-x64.zip
+sudo unzip sapcc-2.12.4-linux-x64.zip
 
-sudo rpm -i com.sap.scc-ui-2.12.0-15.x86_64.rpm
+sudo rpm -i com.sap.scc-ui-2.12.4-4.x86_64.rpm
 
 sleep 10
 
